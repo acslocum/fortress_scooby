1e1b09881ae74dc363790f58e11378f641163e83
acslocum <slocum@gmail.com>
2017-12-04 22:34:27 -0500

    fixing resource leak, adding shuffle-based play

---
+++ Scooby.java
@@ -0,0 +1,109 @@
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Stack;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.StackPane;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.MediaView;
+import javafx.stage.Stage;
+
+public class Scooby extends Application {
+
+	public static void main(String[] args) {
+		Application.launch(args);
+	}
+
+	static boolean playingRandom = false;
+	static MediaPlayer baselinePlayer;
+	static MediaPlayer randomPlayer;
+	List<File> videos;
+	Stack<File> currentVideos;
+
+	
+	
+	@Override
+	public void init() throws Exception {
+		File[] allFiles = new File("./villainVideos/").listFiles();
+		videos = villainVideoList(allFiles);
+	}
+
+	@Override
+	public void start(Stage primaryStage) {
+		System.getProperties().setProperty("javafx.animation.framerate", "30");
+		StackPane root = new StackPane();
+		MediaView mediaView = new MediaView(getBaselineVideo());
+		root.getChildren().add(mediaView);
+		Scene scene = new Scene(root, 1024, 768);
+
+		scene.addEventFilter(KeyEvent.KEY_TYPED, keyEvent -> {
+			if (!Scooby.playingRandom) {
+				Scooby.playingRandom = true;
+				getBaselineVideo().stop();
+				Scooby.randomPlayer = getRandomVideo(mediaView);
+				mediaView.setMediaPlayer(Scooby.randomPlayer);
+				Scooby.randomPlayer.play();
+			}
+		});
+
+		primaryStage.setScene(scene);
+		primaryStage.show();
+		getBaselineVideo().play();
+	}
+	
+	public void resetVideos() {
+		currentVideos = new Stack<File>();
+		List<File> files = new ArrayList<File>(videos);
+		Collections.shuffle(files);
+		for(File file : files) {
+			currentVideos.push(file);
+		}
+	}
+	
+	public File nextVideo() {
+		if(currentVideos==null || currentVideos.isEmpty()) {
+			resetVideos();
+		}
+		return currentVideos.pop();
+	}
+
+	private static MediaPlayer getBaselineVideo() {
+		if (Scooby.baselinePlayer == null) {
+			Scooby.baselinePlayer = new MediaPlayer(
+					new Media(Scooby.class.getResource("baseline.mp4").toExternalForm()));
+			Scooby.baselinePlayer.setCycleCount(MediaPlayer.INDEFINITE);
+		}
+		return Scooby.baselinePlayer;
+
+	}
+
+	private MediaPlayer getRandomVideo(MediaView mediaView) {
+		Scooby.randomPlayer = new MediaPlayer(
+				new Media(Scooby.class.getResource(nextVideo().getPath()).toExternalForm()));
+		randomPlayer.setOnEndOfMedia(new Runnable() {
+			@Override
+			public void run() {
+				Scooby.randomPlayer.dispose();
+				Scooby.playingRandom = false;
+				mediaView.setMediaPlayer(getBaselineVideo());
+				getBaselineVideo().play();
+
+			}
+		});
+		return randomPlayer;
+	}
+
+	private List<File> villainVideoList(File[] allFiles) {
+		List<File> videos = new ArrayList<File>();
+		for (int i = 0; i < allFiles.length; i++)
+			if (allFiles[i].getName().contains(".mp4"))
+				videos.add(allFiles[i]);
+		return videos;
+	}
+
+}
